90e5a28dac36c0d7292e18cf1f96d7a923e1cde5
Jim Wheaton <jwheaton@rickhanseninstitute.org>
2017-05-03 22:03:30 +0000

    upload moved to toolbar

--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FileUploader, FileItem } from 'ng2-file-upload';
 import { LoaderService } from '../shared/loader/loader.service';
 import { environment } from '../../environments/environment';
@@ -11,36 +11,35 @@ import { environment } from '../../environments/environment';
 export class UploaderComponent implements OnInit {
   uploader: FileUploader;
   hasFileOverDropZone: boolean = false;
+  isUploadDisabled: boolean = false;
+
+  @ViewChild('uploadEl') uploadElRef: ElementRef
 
   constructor(private loaderService: LoaderService) {
     var api = `${environment.api}api/stat/import`;
-    this.uploader = new FileUploader({ url: api })
+    this.uploader = new FileUploader({ url: api, queueLimit: 1, autoUpload: true })
   }
-
-  
 
   ngOnInit() {
     this.uploader.onCompleteAll = () => {
+      this.isUploadDisabled = false;
       this.loaderService.hide();
+      this.uploader.clearQueue();
     }
+    this.uploader.onBeforeUploadItem = () => {
+      this.isUploadDisabled = true;
+      this.loaderService.show();
+    }
+  }
+
+  ngAfterViewInit() {
+    this.uploader.onAfterAddingFile = () => {
+      this.uploadElRef.nativeElement.value = ''
+    };
   }
 
   fileOverDropZone(e: any): void {
     this.hasFileOverDropZone = e;
-  }
-
-  uploadButtonDisabled() {
-    var q = this.uploader.queue;
-    return !q.length
-      || q[0].isReady
-      || q[0].isUploading
-      || q[0].isSuccess;
-  }
-
-  upload() {
-    var q = this.uploader.queue;
-    this.loaderService.show();
-    return q.length ? q[0].upload() : (noop) => noop;
   }
 
 }
